635841e3a70f67ccfbfda18645924f3425adefc2
Newton <newtoljunior@hotmail.com>
2020-05-27 16:24:47 -0300

    ajustei a parte de titulos e navegacoes

--- app/src/main/java/br/com/newton/transparente/MainActivity.kt
+++ app/src/main/java/br/com/newton/transparente/MainActivity.kt
@@ -1,12 +1,31 @@
 package br.com.newton.transparente
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.itensFragment, R.id.detalheFragment, R.id.dadosFragment, R.id.ajustesFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        btnNavView.setupWithNavController(navController)
+
+
     }
 }
