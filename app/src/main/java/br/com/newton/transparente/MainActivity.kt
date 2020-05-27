45f838f60e93d13b3d5eb84184f951a828819abe
Newton <newtoljunior@hotmail.com>
2020-05-27 15:10:00 -0300

    Iniciando o projeto construindo a primeira tela

---
+++ app/src/main/java/br/com/newton/transparente/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.newton.transparente
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/br/com/newton/transparente/fragments/itens/ItensViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.newton.transparente.fragments.itens
+
+import androidx.lifecycle.ViewModel
+
+class ItensViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
