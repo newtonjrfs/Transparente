bf8b35e0fe6c97d5878b282583277707c2c9275c
Newton <newtoljunior@hotmail.com>
2020-05-30 10:59:40 -0300

    tirei o view model da tela de ajustes que nao iria usar e criei o progress bar da tela de dados

--- app/src/main/java/br/com/newton/transparente/fragments/ajustes/AjustesFragment.kt
+++ app/src/main/java/br/com/newton/transparente/fragments/ajustes/AjustesFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import br.com.newton.transparente.R
 
 class AjustesFragment : Fragment() {
@@ -14,19 +13,11 @@ class AjustesFragment : Fragment() {
         fun newInstance() = AjustesFragment()
     }
 
-    private lateinit var viewModel: AjustesViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.ajustes_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AjustesViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
--- app/src/main/java/br/com/newton/transparente/fragments/ajustes/AjustesViewModel.kt
+++
@@ -1,7 +0,0 @@
-package br.com.newton.transparente.fragments.ajustes
-
-import androidx.lifecycle.ViewModel
-
-class AjustesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
